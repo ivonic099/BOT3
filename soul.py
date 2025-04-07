97c251a0fc516fcbacae11d5dd5cf76940d25cd6
ivonic099 <glitchv97@gmail.com>
2025-04-08 02:32:47 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -8,7 +8,7 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # Insert your Telegram bot token here
-bot = telebot.TeleBot('7750075604:AAF_OSoByCsmyZKT14jAl6g7UuiGT-0Mc6k')
+bot = telebot.TeleBot('7518472795:AAFtzlmJJ-LYC7MnVNpvKxxlatbBm41T10E')
 
 # Admin user IDs
 admin_id = {"2057365092"}
